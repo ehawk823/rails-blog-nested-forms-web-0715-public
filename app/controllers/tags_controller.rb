79c297296ce433de2f4fa2483fa74011dfd017af
ehawk823 <evan.alexander.hawk@gmail.com>
2015-09-03 17:12:45 -0500

    done

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,10 +6,20 @@ class TagsController < ApplicationController
     @tags = Tag.all
   end
 
+  def create
+    binding.pry
+    Tag.create(tag_params)
+    redirect_to
+  end
+
   # GET /tags/1
   # GET /tags/1.json
   def show
     @tag = Tag.find(params[:id])
+  end
+
+  def tag_params
+    params.require(:user).permit(:name)
   end
 
 end
